21033dacdd88727d1f4cc1d30ddffa4dd444484b
Arley Padua <arleydf@gmail.com>
2021-05-11 11:08:42 +0200

    adds place order tests

---
+++ src/test/kotlin/com/ordersinkotlin/ordersinkotlin/features/PlaceOrderTests.kt
@@ -0,0 +1,44 @@
+package com.ordersinkotlin.ordersinkotlin.features
+
+import com.ordersinkotlin.ordersinkotlin.domain.Order
+import com.ordersinkotlin.ordersinkotlin.domain.OrderFixture
+import com.ordersinkotlin.ordersinkotlin.domain.OrderStatus
+import com.ordersinkotlin.ordersinkotlin.domain.OrdersRepository
+import com.ordersinkotlin.ordersinkotlin.seedwork.CommandHandler
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import java.util.*
+
+@SpringBootTest
+class PlaceOrderTests {
+
+    @Autowired
+    lateinit var handler: CommandHandler.WithoutResult<PlaceOrder.Command>
+
+    @Autowired
+    lateinit var repository: OrdersRepository
+
+    @Test
+    fun `given existing order, when placing, should be placed`() {
+        val order = createOrder()
+
+        val result = runBlocking {
+            handler.handle(PlaceOrder.Command(order.id))
+        }
+
+        val placed = repository.findById(order.id)
+            .get()
+
+        assertThat(placed.status).isEqualTo(OrderStatus.Placed)
+    }
+
+    private fun createOrder(): Order {
+        val order = OrderFixture.getDraftOrder(true)
+        repository.save(order)
+
+        return order
+    }
+}
