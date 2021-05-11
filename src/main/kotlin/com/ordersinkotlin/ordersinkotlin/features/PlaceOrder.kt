636f3873966167874e94bf0b6179c4ea4bec0c36
Arley Padua <arleydf@gmail.com>
2021-05-11 11:03:47 +0200

    implements place order

--- src/main/kotlin/com/ordersinkotlin/ordersinkotlin/features/PlaceOrder.kt
+++ src/main/kotlin/com/ordersinkotlin/ordersinkotlin/features/PlaceOrder.kt
@@ -1,21 +1,28 @@
 package com.ordersinkotlin.ordersinkotlin.features
 
+import com.ordersinkotlin.ordersinkotlin.domain.OrdersRepository
 import com.ordersinkotlin.ordersinkotlin.features.data.OrderProduct
 import com.ordersinkotlin.ordersinkotlin.seedwork.CommandHandler
 import com.ordersinkotlin.ordersinkotlin.seedwork.UnitOfWork
+import com.ordersinkotlin.ordersinkotlin.seedwork.commitTo
 import org.springframework.stereotype.Component
 import java.util.*
 
 class PlaceOrder {
-    data class Command(val orderId: UUID)
-    object Result
+    data class Command(val orderId: String)
 
     @Component
     class Handler(
-//    private val uow: UnitOfWork
-    ) : CommandHandler<Command, Result> {
-        override suspend fun handle(command: Command) : Result {
-            TODO("Not yet implemented")
+        private val uow: UnitOfWork,
+        private val repository: OrdersRepository
+    ) : CommandHandler.WithoutResult<Command> {
+        override suspend fun handle(command: Command) {
+            val order = repository.findById(command.orderId)
+                .orElseGet { throw IllegalArgumentException("Order does not exist") }
+
+            order.place()
+
+            uow.commitTo<OrdersRepository>(order)
         }
     }
 }
