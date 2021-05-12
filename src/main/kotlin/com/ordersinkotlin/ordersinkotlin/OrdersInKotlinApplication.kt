3d7db81ee5bcdcd870b11fe6a7d0e0becaf31484
Arley Padua <arleydf@gmail.com>
2021-05-12 22:32:56 +0200

    applies refactoring

--- src/main/kotlin/com/ordersinkotlin/ordersinkotlin/OrdersInKotlinApplication.kt
+++ src/main/kotlin/com/ordersinkotlin/ordersinkotlin/OrdersInKotlinApplication.kt
@@ -1,10 +1,7 @@
 package com.ordersinkotlin.ordersinkotlin
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.support.beans
-import org.springframework.web.reactive.function.server.coRouter
 
 @SpringBootApplication
 class OrdersInKotlinApplication
